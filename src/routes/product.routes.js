b803a224b6e774102b8324a97fb3e7cc19a7f2de
Rodrigo <rodrigofgiglio@gmail.com>
2020-06-25 15:41:07 -0300

    adds auth verification to each request besides login and register

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -8,12 +8,12 @@ const { validateParam, schemas } = require('../helpers/route.helper')
 
 routes.get('/products', verify, ProductController.index);
 routes.get('/products/:id', verify, validateParam(schemas.idSchema, 'id'), ProductController.show);
-routes.post('/products', ProductController.store);
-routes.put('/products/:id', validateParam(schemas.idSchema, 'id'), ProductController.update);
-routes.delete('/products/:id', validateParam(schemas.idSchema, 'id'), ProductController.destroy);
+routes.post('/products', verify, ProductController.store);
+routes.put('/products/:id', verify, validateParam(schemas.idSchema, 'id'), ProductController.update);
+routes.delete('/products/:id', verify, validateParam(schemas.idSchema, 'id'), ProductController.destroy);
 
-routes.get('/products/:id/buyers', validateParam(schemas.idSchema, 'id'), ProductController.indexBuyers);
-routes.post('/products/:id/buyers', validateParam(schemas.idSchema, 'id'), ProductController.storeBuyer);
-routes.delete('/products/:id/buyers/:buyerId', validateParam(schemas.idSchema, 'id'), validateParam(schemas.idSchema, 'buyerId'),  ProductController.destroyBuyer);
+routes.get('/products/:id/buyers', verify, validateParam(schemas.idSchema, 'id'), ProductController.indexBuyers);
+routes.post('/products/:id/buyers', verify, validateParam(schemas.idSchema, 'id'), ProductController.storeBuyer);
+routes.delete('/products/:id/buyers/:buyerId', verify, validateParam(schemas.idSchema, 'id'), validateParam(schemas.idSchema, 'buyerId'),  ProductController.destroyBuyer);
 
  module.exports = routes;
